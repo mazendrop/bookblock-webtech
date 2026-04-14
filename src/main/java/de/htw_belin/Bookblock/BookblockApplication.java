7052360246d6bfe1287b2c84439104d9e64215d2
mazennasfat <mazen8abdallah@gmail.com>
2026-04-14 18:18:40 +0200

    generated backend

---
+++ src/main/java/de/htw_belin/Bookblock/BookblockApplication.java
@@ -0,0 +1,13 @@
+package de.htw_belin.Bookblock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookblockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookblockApplication.class, args);
+	}
+
+}
